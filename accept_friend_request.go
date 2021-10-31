f2fe6f8efdd9905877a0c974d3ba8545cbad02fc
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-10-30 21:48:38 -0400

    sendFriendRequestAcceptedNotification

--- accept_friend_request.go
+++ accept_friend_request.go
@@ -8,6 +8,25 @@ import (
 func acceptFriendRequest(err error, client *nex.Client, callID uint32, id uint64) {
 	friendInfo := acceptFriendshipAndReturnFriendInfo(id)
 
+	friendPID := friendInfo.NNAInfo.PrincipalBasicInfo.PID
+	connectedUser := connectedUsers[friendPID]
+
+	if connectedUser != nil {
+		senderPID := client.PID()
+		senderConnectedUser := connectedUsers[senderPID]
+
+		senderFriendInfo := nexproto.NewFriendInfo()
+
+		senderFriendInfo.NNAInfo = senderConnectedUser.NNAInfo
+		senderFriendInfo.Presence = senderConnectedUser.Presence
+		senderFriendInfo.Status = getUserComment(senderPID)
+		senderFriendInfo.BecameFriend = friendInfo.BecameFriend
+		senderFriendInfo.LastOnline = friendInfo.LastOnline // TODO: Change this
+		senderFriendInfo.Unknown = 0
+
+		go sendFriendRequestAcceptedNotification(connectedUser.Client, senderFriendInfo)
+	}
+
 	rmcResponseStream := nex.NewStreamOut(nexServer)
 
 	rmcResponseStream.WriteStructure(friendInfo)
@@ -32,4 +51,37 @@ func acceptFriendRequest(err error, client *nex.Client, callID uint32, id uint64
 	responsePacket.AddFlag(nex.FlagReliable)
 
 	nexServer.Send(responsePacket)
+}
+
+func sendFriendRequestAcceptedNotification(client *nex.Client, friendInfo *nexproto.FriendInfo) {
+	eventObject := nexproto.NewNintendoNotificationEvent()
+	eventObject.Type = 30
+	eventObject.SenderPID = friendInfo.NNAInfo.PrincipalBasicInfo.PID
+	eventObject.DataHolder = nex.NewDataHolder()
+	eventObject.DataHolder.Name = "FriendInfo"
+	eventObject.DataHolder.Object = friendInfo
+
+	stream := nex.NewStreamOut(nexServer)
+	eventObjectBytes := eventObject.Bytes(stream)
+
+	rmcRequest := nex.NewRMCRequest()
+	rmcRequest.SetProtocolID(nexproto.NintendoNotificationsProtocolID)
+	rmcRequest.SetCallID(3810693103)
+	rmcRequest.SetMethodID(nexproto.NintendoNotificationsMethodProcessNintendoNotificationEvent1)
+	rmcRequest.SetParameters(eventObjectBytes)
+
+	rmcRequestBytes := rmcRequest.Bytes()
+
+	requestPacket, _ := nex.NewPacketV0(client, nil)
+
+	requestPacket.SetVersion(0)
+	requestPacket.SetSource(0xA1)
+	requestPacket.SetDestination(0xAF)
+	requestPacket.SetType(nex.DataPacket)
+	requestPacket.SetPayload(rmcRequestBytes)
+
+	requestPacket.AddFlag(nex.FlagNeedsAck)
+	requestPacket.AddFlag(nex.FlagReliable)
+
+	nexServer.Send(requestPacket)
 }
