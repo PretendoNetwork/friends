441e5c168f0b08398c4f1fe96045e166775f3e4a
Daniel López Guimaraes <danielectra@outlook.com>
2023-06-28 18:35:50 +0100

    Add 3DS friends to GetUserFriendPIDs gRPC method
    Only add complete friendships to the friend list.

--- grpc/get_user_friend_pids.go
+++ grpc/get_user_friend_pids.go
@@ -4,12 +4,31 @@ import (
 	"context"
 
 	database_wiiu "github.com/PretendoNetwork/friends-secure/database/wiiu"
+	database_3ds "github.com/PretendoNetwork/friends-secure/database/3ds"
 	pb "github.com/PretendoNetwork/grpc-go/friends"
 )
 
 func (s *gRPCFriendsServer) GetUserFriendPIDs(ctx context.Context, in *pb.GetUserFriendPIDsRequest) (*pb.GetUserFriendPIDsResponse, error) {
+	var pids []uint32
 
-	pids := database_wiiu.GetUserFriendPIDs(in.GetPid())
+	// * Try Wii U database first
+	pids = database_wiiu.GetUserFriendPIDs(in.GetPid())
+
+	if len(pids) > 0 {
+		return &pb.GetUserFriendPIDsResponse{
+			Pids: pids,
+		}, nil
+	}
+
+	// * If no PIDs are given, try with 3DS database instead
+	relationships := database_3ds.GetUserFriends(in.GetPid())
+
+	for _, relationship := range relationships {
+		// * Only add complete relationships to the list
+		if relationship.RelationshipType == 1 {
+			pids = append(pids, relationship.PID)
+		}
+	}
 
 	return &pb.GetUserFriendPIDsResponse{
 		Pids: pids,
