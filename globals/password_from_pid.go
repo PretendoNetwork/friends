84db1463eb637795cd58e3a0b14ea0d40427d27d
SuperMarioDaBom <ajguerra2001@gmail.com>
2023-07-17 18:36:19 -0700

    Switch to newer function

--- globals/password_from_pid.go
+++ globals/password_from_pid.go
@@ -12,7 +12,7 @@ import (
 func PasswordFromPID(pid uint32) (string, uint32) {
 	ctx := metadata.NewOutgoingContext(context.Background(), GRPCAccountCommonMetadata)
 
-	response, err := GRPCAccountClient.GetNEXPassword(ctx, &pb.GetNEXPasswordRequest{Pid: pid})
+	response, err := GRPCAccountClient.GetNEXData(ctx, &pb.GetNEXDataRequest{Pid: pid})
 	if err != nil {
 		globals.Logger.Error(err.Error())
 		return "", nex.Errors.RendezVous.InvalidUsername
