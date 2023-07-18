1779a4c72707f46a55b30463120d9579448f9e8b
SuperMarioDaBom <ajguerra2001@gmail.com>
2023-07-18 11:40:36 -0700

    Add RegisterEx override, remove unneeded Register function

--- nex/register_common_secure_server_protocols.go
+++ nex/register_common_secure_server_protocols.go
@@ -2,9 +2,11 @@ package nex
 
 import (
 	"github.com/PretendoNetwork/friends-secure/globals"
+	nex_secure_connection "github.com/PretendoNetwork/friends-secure/nex/secure-connection"
 	secureconnection "github.com/PretendoNetwork/nex-protocols-common-go/secure-connection"
 )
 
 func registerCommonSecureServerProtocols() {
-	secureconnection.NewCommonSecureConnectionProtocol(globals.SecureServer)
+	secureConnectionProtocol := secureconnection.NewCommonSecureConnectionProtocol(globals.SecureServer)
+	secureConnectionProtocol.RegisterEx(nex_secure_connection.RegisterEx)
 }
