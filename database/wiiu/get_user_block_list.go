71f47f9ffc9771d41646bb45685793e550379ee8
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2024-06-23 13:42:55 +0100

    fix: remove n+1 queries

--- database/wiiu/get_user_block_list.go
+++ database/wiiu/get_user_block_list.go
@@ -13,7 +13,16 @@ func GetUserBlockList(pid uint32) (*types.List[*friends_wiiu_types.BlacklistedPr
 	blockList := types.NewList[*friends_wiiu_types.BlacklistedPrincipal]()
 	blockList.Type = friends_wiiu_types.NewBlacklistedPrincipal()
 
-	rows, err := database.Manager.Query(`SELECT blocked_pid, title_id, title_version, date FROM wiiu.blocks WHERE blocker_pid=$1`, pid)
+	rows, err := database.Manager.Query(`
+	SELECT
+		b.blocked_pid, b.title_id, b.title_version, b.date,
+		bi.username, bi.unknown,
+		mii.name, mii.unknown1, mii.unknown2, mii.data, mii.unknown_datetime
+	FROM wiiu.blocks AS b
+	INNER JOIN wiiu.principal_basic_info AS bi ON bi.pid = b.blocked_pid
+	INNER JOIN wiiu.mii AS mii ON mii.pid = b.blocked_pid
+	WHERE blocker_pid=$1
+	`, pid)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return blockList, database.ErrBlacklistNotFound
@@ -24,25 +33,40 @@ func GetUserBlockList(pid uint32) (*types.List[*friends_wiiu_types.BlacklistedPr
 	defer rows.Close()
 
 	for rows.Next() {
-		var pid uint32
+		var blockedPID uint32
 		var titleID uint64
 		var titleVersion uint16
 		var date uint64
 
-		err := rows.Scan(&pid, &titleID, &titleVersion, &date)
+		var blockedNNID string
+		var unknown uint8
+
+		var miiName string
+		var miiUnknown1 uint8
+		var miiUnknown2 uint8
+		var miiData []byte
+		var miiDatetime uint64
+
+		err := rows.Scan(&blockedPID, &titleID, &titleVersion, &date, &blockedNNID, &unknown, &miiName, &miiUnknown1, &miiUnknown2, &miiData, &miiDatetime)
 		if err != nil {
 			return nil, err
 		}
 
-		userInfo, err := GetUserPrincipalBasicInfo(pid)
-		if err != nil {
-			return nil, err
-		}
+		mii := friends_wiiu_types.NewMiiV2()
+		mii.Name = types.NewString(miiName)
+		mii.Unknown1 = types.NewPrimitiveU8(miiUnknown1)
+		mii.Unknown2 = types.NewPrimitiveU8(miiUnknown2)
+		mii.MiiData = types.NewBuffer(miiData)
+		mii.Datetime = types.NewDateTime(miiDatetime)
+
+		principalBasicInfo := friends_wiiu_types.NewPrincipalBasicInfo()
+		principalBasicInfo.PID = types.NewPID(uint64(blockedPID))
+		principalBasicInfo.NNID = types.NewString(blockedNNID)
+		principalBasicInfo.Unknown = types.NewPrimitiveU8(unknown)
+		principalBasicInfo.Mii = mii
 
 		blacklistPrincipal := friends_wiiu_types.NewBlacklistedPrincipal()
-
-		blacklistPrincipal.PrincipalBasicInfo = userInfo
-
+		blacklistPrincipal.PrincipalBasicInfo = principalBasicInfo
 		blacklistPrincipal.GameKey = friends_wiiu_types.NewGameKey()
 		blacklistPrincipal.GameKey.TitleID = types.NewPrimitiveU64(titleID)
 		blacklistPrincipal.GameKey.TitleVersion = types.NewPrimitiveU16(titleVersion)
--- database/wiiu/get_user_friend_requests_out.go
+++ database/wiiu/get_user_friend_requests_out.go
@@ -13,7 +13,17 @@ func GetUserFriendRequestsOut(pid uint32) (*types.List[*friends_wiiu_types.Frien
 	friendRequests := types.NewList[*friends_wiiu_types.FriendRequest]()
 	friendRequests.Type = friends_wiiu_types.NewFriendRequest()
 
-	rows, err := database.Manager.Query(`SELECT id, recipient_pid, sent_on, expires_on, message, received FROM wiiu.friend_requests WHERE sender_pid=$1 AND accepted=false`, pid)
+	rows, err := database.Manager.Query(`
+	SELECT
+		fr.id, fr.recipient_pid, fr.sent_on, fr.expires_on, fr.message, fr.received,
+		bi.username, bi.unknown,
+		mii.name, mii.unknown1, mii.unknown2, mii.data, mii.unknown_datetime
+	FROM wiiu.friend_requests AS fr
+	INNER JOIN wiiu.principal_basic_info AS bi ON bi.pid = fr.recipient_pid
+	INNER JOIN wiiu.mii AS mii ON mii.pid = fr.recipient_pid
+	WHERE sender_pid=$1 AND accepted=false
+	LIMIT 100
+	`, pid)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return friendRequests, database.ErrEmptyList
@@ -31,19 +41,35 @@ func GetUserFriendRequestsOut(pid uint32) (*types.List[*friends_wiiu_types.Frien
 		var message string
 		var received bool
 
-		err := rows.Scan(&id, &recipientPID, &sentOn, &expiresOn, &message, &received)
+		var recipientNNID string
+		var unknown uint8
+
+		var miiName string
+		var miiUnknown1 uint8
+		var miiUnknown2 uint8
+		var miiData []byte
+		var miiDatetime uint64
+
+		err := rows.Scan(&id, &recipientPID, &sentOn, &expiresOn, &message, &received, &recipientNNID, &unknown, &miiName, &miiUnknown1, &miiUnknown2, &miiData, &miiDatetime)
 		if err != nil {
 			return friendRequests, err
 		}
 
-		userInfo, err := GetUserPrincipalBasicInfo(recipientPID)
-		if err != nil {
-			return nil, err
-		}
+		mii := friends_wiiu_types.NewMiiV2()
+		mii.Name = types.NewString(miiName)
+		mii.Unknown1 = types.NewPrimitiveU8(miiUnknown1)
+		mii.Unknown2 = types.NewPrimitiveU8(miiUnknown2)
+		mii.MiiData = types.NewBuffer(miiData)
+		mii.Datetime = types.NewDateTime(miiDatetime)
+
+		principalBasicInfo := friends_wiiu_types.NewPrincipalBasicInfo()
+		principalBasicInfo.PID = types.NewPID(uint64(recipientPID))
+		principalBasicInfo.NNID = types.NewString(recipientNNID)
+		principalBasicInfo.Unknown = types.NewPrimitiveU8(unknown)
+		principalBasicInfo.Mii = mii
 
 		friendRequest := friends_wiiu_types.NewFriendRequest()
-
-		friendRequest.PrincipalInfo = userInfo
+		friendRequest.PrincipalInfo = principalBasicInfo
 		friendRequest.Message = friends_wiiu_types.NewFriendRequestMessage()
 		friendRequest.Message.FriendRequestID = types.NewPrimitiveU64(id)
 		friendRequest.Message.Received = types.NewPrimitiveBool(received)
